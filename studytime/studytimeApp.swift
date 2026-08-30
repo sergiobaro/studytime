88cd505f5aafe37c24cd9eb4d8e14f9a38f5ba3d
serbarsim@gmail.com <Sergio Bar>
2026-08-30 14:51:36 +0200

    Initial Commit

---
+++ studytime/studytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  studytimeApp.swift
+//  studytime
+//
+//  Created by Sergio Baro on 30/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct studytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
