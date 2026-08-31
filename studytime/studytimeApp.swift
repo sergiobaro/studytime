aa3a2d2965d3203a369a6393037b184a33d46ae1
serbarsim@gmail.com <Sergio Baro>
2026-08-31 09:37:32 +0200

    Added timer

--- studytime/studytimeApp.swift
+++ studytime/studytimeApp.swift
@@ -1,14 +1,7 @@
-//
-//  studytimeApp.swift
-//  studytime
-//
-//  Created by Sergio Baro on 30/08/2026.
-//
-
 import SwiftUI
 
 @main
-struct studytimeApp: App {
+struct StudyTimeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
