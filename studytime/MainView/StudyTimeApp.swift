381cccb4b614fe58b98f3876f5e2259b63008528
Sergio Baro <sergio.baro@proton.me>
2026-09-11 08:19:05 +0200

    Removed support for visionOS, and disabled windows and tabs

--- studytime/MainView/StudyTimeApp.swift
+++ studytime/MainView/StudyTimeApp.swift
@@ -2,10 +2,23 @@ import SwiftUI
 
 @main
 struct StudyTimeApp: App {
+    #if os(macOS)
+    init() {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
+    #endif
+
     var body: some Scene {
-        WindowGroup {
+        #if os(macOS)
+        Window("StudyTime", id: "main") {
             ContentView()
         }
         .windowStyle(.hiddenTitleBar)
+        #else
+        WindowGroup {
+            ContentView()
+        }
+        .commandsRemoved()
+        #endif
     }
 }
